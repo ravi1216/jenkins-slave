a2ed7334bb34716a22c16b8789f3d5ff0f297ca4
ravi <ravits0330@gmail.com>
2018-06-11 20:45:17 +0000

    jenkins

---
+++ slave/cmd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Do some stuffs
+
+exec /usr/sbin/init # To correctly start D-Bus thanks to https://forums.docker.com/t/any-simple-and-safe-way-to-start-services-on-centos7-systemd/5695/8
+
